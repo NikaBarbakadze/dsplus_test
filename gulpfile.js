be88eaa6ce30cd3d8ad63ff70ffcadbef7e7ce69
NikaBarbakadze <45419134+NikaBarbakadze@users.noreply.github.com>
2019-03-07 10:43:52 -0800

    Added new items

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('scss/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 3 version'],
+        cascade: false
+}))
+    .pipe(gulp.dest('styles'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', gulp.parallel('sass'));
+});
